f7389f3f6ce1b2348e753edf7525b2f0ca1fe2e0
leandroArraes <leandro.arraes.182@gmail.com>
2023-05-11 20:49:55 -0300

    Deu certo a concexão do Banco

--- src/bancoJava/JdbcTeste.java
+++ src/bancoJava/JdbcTeste.java
@@ -4,13 +4,14 @@ import java.sql.SQLException;
 
 public class JdbcTeste {
 	public static void main(String[] args) {
-		String url = "jdbc://mysql://localhost/test";
+		String url = "jdbc:mysql://localhost:3306/test?useTimezone=true&serverTimezone=UTC";
 		String usuario = "root";
 		String senha = "";
 		
-									
+										
 			try {
 				DriverManager.getConnection(url,usuario,senha);
+				System.out.println("Deu certo a conexão do Banco");
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
